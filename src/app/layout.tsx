733cf961646c5c0e03050f08d33eceaca9da12d9
Julio César Serrano Sandoval <79113995+js3rr4n0@users.noreply.github.com>
2026-06-19 06:37:31 -0600

    feat: add @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="container" style={{ padding: '32px 24px' }}>
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
